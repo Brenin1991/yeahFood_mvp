d3c4908f1b7388a5f82f264bad1fad9ac16e9e59
Breno Campos Ribeiro <brenocamposr@gmail.com>
2019-11-06 18:50:24 -0300

    Ver Cardápio

---
+++ app/Controllers/Http/CardapioController.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const Cardapio = use('App/Models/Cardapio');
+const Database = use('Database');
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with cardapios
+ */
+class CardapioController {
+  /**
+   * Show a list of all cardapios.
+   * GET cardapios
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const cardapio = (await Database.select('*').from('cardapios'));
+    console.log(cardapio);
+    return view.render('home', { cardapio });
+  }
+
+  /**
+   * Render a form to be used for creating a new cardapio.
+   * GET cardapios/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new cardapio.
+   * POST cardapios
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single cardapio.
+   * GET cardapios/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing cardapio.
+   * GET cardapios/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update cardapio details.
+   * PUT or PATCH cardapios/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a cardapio with id.
+   * DELETE cardapios/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = CardapioController
